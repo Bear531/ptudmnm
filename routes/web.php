6fead832281e0d39c79d10669099ba7994cf67c6
Bear531 <luutrinhgiayen2@gmail.com>
2026-03-24 16:52:48 +0700

    tải breeze

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/yen', function () {
     return "Hello Yen"; #dòng này để cố ý tạo xung đột
 });
@@ -28,3 +41,14 @@ route::get('/runtime', 'App\Http\Controllers\btn@runtime');
 #btn 2
 Route::get('/sach/{id?}', 'App\Http\Controllers\ViDuLayoutController@sach'); // ? là chỉ định id có thể có hoặc không (tuỳ chọn)
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
